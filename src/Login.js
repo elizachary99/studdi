107192cda6622339494003d93ddad91ed032663f
elizachary99 <elizachary99@gmail.com>
2022-04-18 20:25:07 -0400

    Initial commit

---
+++ src/Login.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import {Button, FormControl, Stack, TextField} from '@mui/material'
+import {useNavigate} from 'react-router-dom'
+
+function Login(){
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [loginResult, setLoginResult] = useState("\u00A0")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setLoginResult("Username and password do not match any existing users")
+    }
+
+    const navigate = useNavigate()
+
+    return(
+        <Stack id="login-panel" direction="column" alignItems="center">
+            <img id="studdi-logo" alt="Studdi" src="/Studdi-logos_transparent 1.png" style={{width: "50%"}}></img><br />
+            <form onSubmit={handleSubmit}>
+                <Stack id="login-form" direction="column" alignItems="center">
+                    <TextField id="username" label='Username' required
+                        value={username} onChange={e => setUsername(e.target.value)} /><br />
+                    <TextField id='password' label='Password' type='password' required
+                        value={password} onChange={e => setPassword(e.target.value)} /><br />
+                    <Button id="log-in" variant="contained" type="submit">Log In</Button>
+                    <div style={{color: "red"}}>{loginResult}</div>
+                </Stack>
+            </form>
+            <br />
+            <div>Don't have an account?</div>
+            <Button variant="contained" onClick={() => {navigate("/signup")}}>Sign Up</Button>
+        </Stack>
+    )
+}
+
+export default Login;
---
+++ src/Signup.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import {Button, FormControl, Stack, TextField} from '@mui/material'
+import {useNavigate} from 'react-router-dom'
+
+function Signup(){
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [error, setError] = useState("\u00A0")
+
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(password != confirmPassword) setError("Passwords must match");
+        else navigate("/editProfile")
+    }
+
+    return(
+        <Stack id="login-panel" direction="column" alignItems="center">
+            <div>Create Account</div><br />
+            <form onSubmit={handleSubmit}>
+                <Stack id="login-form" direction="column" alignItems="center">
+                    <TextField id="username" label='Username' required
+                        value={username} onChange={e => setUsername(e.target.value)} /><br />
+                    <TextField id='password' label='Password' type='password' required
+                        value={password} onChange={e => setPassword(e.target.value)} /><br />
+                    <TextField id='confirm-password' label='Confirm Password' type='password' required
+                        value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} /><br />
+                    <Button id="log-in" variant="contained" type="submit">Sign Up</Button>
+                    <div id="error-message" style={{color: "red"}}>{error}</div>
+                </Stack>
+            </form>
+        </Stack>
+    )
+}
+
+export default Signup;
