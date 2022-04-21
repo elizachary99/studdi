65e5b32d7c2c838f37bb5b378adb91ea53b80962
Nathan Urquhart <Nurquhart014@gmail.com>
2022-04-20 22:46:14 -0400

    added drop down boxes to 'create profile'

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -8,6 +8,8 @@ function EditProfile() {
     const [last, setLast] = useState("")
     const [birthday, setBirthday] = useState(new Date())
     const [picture, setPicture] = useState()
+    const [studyType, setStudyType] = useState("No Preference")
+    const [studyLocation, setStudyLocation] = useState("No Preference")
     const [error, setError] = useState("\u00A0")
 
     const navigate = useNavigate()
@@ -28,11 +30,23 @@ function EditProfile() {
                     <p>Birthday*</p>
                     <TextField type="date" id="birthday" required
                         value={birthday} onChange={e => setBirthday(e.target.value)} /><br />
-                        
+
                     <h3>Optional Preferences</h3><br />
                     <p>Profile Picture:</p>
-                    <input type="file" accept="image/*" multiple="false" 
+                    <input type="file" accept="image/*" multiple="false"
                         value={picture} onChange={e => setPicture(e.target.value)} /><br />
+                    <p>Would you prefer studying as a pair or with a group?</p>
+                    <select defaultValue= {studyType} name="pairOrGroup" id="pairOrGroup" onChange={e => setStudyType(e.target.textContent)}>
+                        <option value="NoPreference">No Preference</option>
+                        <option value="Pair">Pair</option>
+                        <option value="Group">Group</option>
+                    </select>
+                    <p>Would you prefer studying in person or online?</p>
+                    <select defaultValue= {studyLocation} name="onlineOrInPerson" id="onlineOrInPerson" onChange={e => setStudyLocation(e.target.textContent)}>
+                        <option value="NoPreference">No Preference</option>
+                        <option value="Online">Online</option>
+                        <option value="InPerson">In Person</option>
+                    </select><br />
 
                     <Button id="save-profile" variant="contained" type="submit">Save</Button>
                     <div id="error-message" style={{ color: "red" }}>{error}</div>
