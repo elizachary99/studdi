e7dcd16e46c4165aba38f1b5272a5478d3fe30d2
Nathan Urquhart <Nurquhart014@gmail.com>
2022-04-20 21:50:39 -0400

    added birthday and profile pic to create profile

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -1,10 +1,13 @@
+import React from 'react'
 import { useState } from 'react'
-import {Button, FormControl, Stack, TextField} from '@mui/material'
-import {useNavigate} from 'react-router-dom'
+import { Button, FormControl, Stack, TextField } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
-function EditProfile(){
+function EditProfile() {
     const [first, setFirst] = useState("")
     const [last, setLast] = useState("")
+    const [birthday, setBirthday] = useState(new Date())
+    const [picture, setPicture] = useState()
     const [error, setError] = useState("\u00A0")
 
     const navigate = useNavigate()
@@ -13,17 +16,26 @@ function EditProfile(){
         e.preventDefault()
     }
 
-    return(
+    return (
         <Stack id="profile-panel" direction="column" alignItems="center">
-            <div>Create/Edit Profile</div><br />
+            <h2>Create/Edit Profile</h2><br />
             <form onSubmit={handleSubmit}>
                 <Stack id="profile-form" direction="column" alignItems="center">
                     <TextField id="first" label='First name' required
                         value={first} onChange={e => setFirst(e.target.value)} /><br />
                     <TextField id='last' label='Last name' required
                         value={last} onChange={e => setLast(e.target.value)} /><br />
+                    <p>Birthday*</p>
+                    <TextField type="date" id="birthday" required
+                        value={birthday} onChange={e => setBirthday(e.target.value)} /><br />
+                        
+                    <h3>Optional Preferences</h3><br />
+                    <p>Profile Picture:</p>
+                    <input type="file" accept="image/*" multiple="false" 
+                        value={picture} onChange={e => setPicture(e.target.value)} /><br />
+
                     <Button id="save-profile" variant="contained" type="submit">Save</Button>
-                    <div id="error-message" style={{color: "red"}}>{error}</div>
+                    <div id="error-message" style={{ color: "red" }}>{error}</div>
                 </Stack>
             </form>
         </Stack>
