ed739ab631ec30edaaba6152beca1a2bba66cdae
dannycao10 <59711982+dannycao10@users.noreply.github.com>
2022-04-24 16:26:53 -0400

    Button navigates to edit profile

--- src/EndOfMatches.js
+++ src/EndOfMatches.js
@@ -1,11 +1,13 @@
 import { Button, Stack } from "@mui/material";
+import {useNavigate} from 'react-router-dom'
 
 function EndOfMatches(){
+    const navigate = useNavigate()
     return(
         <Stack>
             <div id="message">You have run out of potential matches based on your applied filters</div>
             <br />
-            <Button id="broaden-search" variant="contained">Broaden your search</Button>
+            <Button id="broaden-search" onClick={() => {navigate("/editProfile")}} variant="contained">Broaden your search</Button>
         </Stack>
     )
 }
