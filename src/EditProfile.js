2e289af8b08d0e77f4805a87b7fbfe30e0a353ac
dannycao10 <59711982+dannycao10@users.noreply.github.com>
2022-04-24 18:05:24 -0400

    edit profile changed

--- src/EditProfile.js
+++ src/EditProfile.js
@@ -2,21 +2,64 @@ import React from 'react'
 import { useState } from 'react'
 import { Button, FormControl, Stack, TextField } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import Select from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
+// import './index.css';
 
 function EditProfile() {
     const [first, setFirst] = useState("")
     const [last, setLast] = useState("")
     const [birthday, setBirthday] = useState(new Date())
     const [picture, setPicture] = useState()
-    const [studyType, setStudyType] = useState("No Preference")
-    const [studyLocation, setStudyLocation] = useState("No Preference")
+    // const [studyType, setStudyType] = useState("No Preference")
+    // const [studyLocation, setStudyLocation] = useState("No Preference")
     const [error, setError] = useState("\u00A0")
+    const [av, setAv] = useState([])
+    const [clas, setClas] = useState([])
+    const [method, setMethod] = useState([])
+
+    const handleChangeA = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setAv(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+
+    const handleChangeC = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setClas(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+
+    const handleChangeM = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setMethod(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
 
     const navigate = useNavigate()
 
     const handleSubmit = (e) => {
         e.preventDefault()
     }
+    const methods = ['Pair', 'Group', 'In Person', 'Online'];
+    const times = ['Mon. Morning', 'Mon. Afternoon', 'Tues. Morning', 'Tues. Afternoon', 'Wed. Morning', 'Wed. Afternoon', 'Thurs. Morning', 'Thurs. Afternoon', 'Fri. Morning', 'Fri. Afternoon', 'Sat. Morning', 'Sat. Afternoon', 'Sun. Morning', 'Sun. Afternoon'];
+    const classes = ['BIOL 101', 'ECON101']
 
     return (
         <Stack id="profile-panel" direction="column" alignItems="center">
@@ -24,18 +67,84 @@ function EditProfile() {
             <form onSubmit={handleSubmit}>
                 <Stack id="profile-form" direction="column" alignItems="center">
                     <TextField id="first" label='First name' required
-                        value={first} onChange={e => setFirst(e.target.value)} /><br />
+                        value={first} onChange={e => setFirst(e.target.value)} sx={{ m: 1, width: 250 }} /><br />
                     <TextField id='last' label='Last name' required
-                        value={last} onChange={e => setLast(e.target.value)} /><br />
+                        value={last} onChange={e => setLast(e.target.value)} sx={{ m: 1, width: 250 }} /><br />
+
                     <p>Birthday*</p>
                     <TextField type="date" id="birthday" required
-                        value={birthday} onChange={e => setBirthday(e.target.value)} /><br />
+                        value={birthday} onChange={e => setBirthday(e.target.value)} sx={{ m: 1, width: 250 }} /><br />
+
+                    <p>Availability*</p>
+                    <FormControl sx={{ m: 1, width: 250, mb: 2 }}>
+                        <InputLabel id="demo-multiple-checkbox-label">Availability</InputLabel>
+                        <Select
+                            labelId="demo-multiple-checkbox-label"
+                            id="demo-multiple-checkbox"
+                            multiple={true}
+                            value={av}
+                            onChange={handleChangeA}
+                            input={<OutlinedInput label="Availability" />}
+                            renderValue={(selected) => selected.join(', ')}
+                        >
+                            {times.map((t) => (
+                                <MenuItem key={t} value={t}>
+                                    <Checkbox checked={av.indexOf(t) > -1} />
+                                    <ListItemText primary={t} />
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
 
                     <h3>Optional Preferences</h3><br />
+
                     <p>Profile Picture:</p>
-                    <input type="file" accept="image/*" multiple="false"
-                        value={picture} onChange={e => setPicture(e.target.value)} /><br />
-                    <p>Would you prefer studying as a pair or with a group?</p>
+                        <input type="file" accept="image/*" multiple="false"
+                            value={picture} onChange={e => setPicture(e.target.value)} />
+                    <br />
+
+                    <p>Classes:</p>
+                    <FormControl sx={{ m: 1, width: 250, mb: 2 }}>
+                        <InputLabel id="demo-multiple-checkbox-label">Classes</InputLabel>
+                        <Select
+                            labelId="demo-multiple-checkbox-label"
+                            id="demo-multiple-checkbox"
+                            multiple={true}
+                            value={clas}
+                            onChange={handleChangeC}
+                            input={<OutlinedInput label="Class" />}
+                            renderValue={(selected) => selected.join(', ')}
+                        >
+                            {classes.map((c) => (
+                                <MenuItem key={c} value={c}>
+                                    <Checkbox checked={clas.indexOf(c) > -1} />
+                                    <ListItemText primary={c} />
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+
+                    <p>Tell us some of your study methods:</p>
+                    <FormControl sx={{ m: 1, width: 250, mb: 2 }}>
+                        <InputLabel id="demo-multiple-checkbox-label">Study Methods</InputLabel>
+                        <Select
+                            labelId="demo-multiple-checkbox-label"
+                            id="demo-multiple-checkbox"
+                            multiple={true}
+                            value={method}
+                            onChange={handleChangeM}
+                            input={<OutlinedInput label="Study Methods" />}
+                            renderValue={(selected) => selected.join(', ')}
+                        >
+                            {methods.map((m) => (
+                                <MenuItem key={m} value={m}>
+                                    <Checkbox checked={method.indexOf(m) > -1} />
+                                    <ListItemText primary={m} />
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                    {/* <p>Would you prefer studying as a pair or with a group?</p>
                     <select defaultValue= {studyType} name="pairOrGroup" id="pairOrGroup" onChange={e => setStudyType(e.target.textContent)}>
                         <option value="NoPreference">No Preference</option>
                         <option value="Pair">Pair</option>
@@ -46,7 +155,7 @@ function EditProfile() {
                         <option value="NoPreference">No Preference</option>
                         <option value="Online">Online</option>
                         <option value="InPerson">In Person</option>
-                    </select><br />
+                    </select><br /> */}
 
                     <Button id="save-profile" variant="contained" type="submit">Save</Button>
                     <div id="error-message" style={{ color: "red" }}>{error}</div>
