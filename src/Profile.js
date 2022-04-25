eb128731ded4f13abaa40dbd13cb54784abc8be6
dannycao10 <59711982+dannycao10@users.noreply.github.com>
2022-04-24 22:20:17 -0400

    Added profile

---
+++ src/Profile.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import {Button, FormControl, Stack, TextField} from '@mui/material'
+import {useNavigate} from 'react-router-dom'
+
+function Profile(){
+
+    const navigate = useNavigate()
+
+    return(
+        <div>Hi</div>
+    )
+}
+
+export default Profile;
