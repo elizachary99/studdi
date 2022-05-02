ac0f711e6fd9f1e566f06c2954680fbbb6d9eb9d
dannycao10 <59711982+dannycao10@users.noreply.github.com>
2022-05-01 20:08:28 -0400

    Update EndOfMatches.js

--- src/EndOfMatches.js
+++ src/EndOfMatches.js
@@ -1,13 +1,18 @@
-import { Button, Stack } from "@mui/material";
+import { Button, Stack, ButtonGroup } from "@mui/material";
 import {useNavigate} from 'react-router-dom'
 
 function EndOfMatches(){
     const navigate = useNavigate()
     return(
-        <Stack>
+        <Stack direction="column" alignItems="center">
             <div id="message">You have run out of potential matches based on your applied filters</div>
             <br />
             <Button id="broaden-search" onClick={() => {navigate("/editProfile")}} variant="contained">Broaden your search</Button>
+            <ButtonGroup variant="contained" sx={{ marginTop: "30px" }} aria-label="outlined primary button group">
+                <Button onClick={() => {navigate("/swipe")}}>Swipe</Button>
+                <Button onClick={() => {navigate("/matches")}} sx={{color:"black"}}>Matches</Button>
+                <Button onClick={() => {navigate("/profile")}} sx={{color:"black"}}>Profile</Button>
+            </ButtonGroup>
         </Stack>
     )
 }
